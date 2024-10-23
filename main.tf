cded6b19f2c2fdca8d4288a3833ac17b47f365a2
nareshgantala <nareshgantala@gmail.com>
2024-10-23 11:36:14 +0530

    secure string

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_ssm_parameter" "foo" {
-  name  = "test.test2"
-  type  = "String"
+  name  = "test.test3"
+  type  = "SecureString"
   value = "bar"
+  key_id = "23c18878-53c4-4477-a7dd-7988c02c72a7"
 }
